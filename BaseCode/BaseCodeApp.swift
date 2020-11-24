80f5ecd3403e782836adae9cc350c6cf4b6f6fc8
perekrist <perekrist31@gmail.com>
2020-11-24 10:48:43 +0700

    Initial Commit

---
+++ BaseCode/BaseCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseCodeApp.swift
+//  BaseCode
+//
+//  Created by Кристина Перегудова on 24.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct BaseCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaseCode
+//
+//  Created by Кристина Перегудова on 24.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
